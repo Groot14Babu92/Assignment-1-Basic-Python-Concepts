ac750964b528b0639182c33f46dffbc622b2ae60
Groot14Babu92 <102485779+Groot14Babu92@users.noreply.github.com>
2025-08-06 12:42:44 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+add = int(a) + int(b)
+subtract = int(a) - int(b)
+multiply = int(a) * int(b)
+divide = int(a) / int(b)
+
+print(f"Addition: {add}")
+print(f"Subtraction: {subtract}")
+print(f"Multiplication: {multiply}")
+print(f"Division: {divide}")
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+fName = input("Enter the first name: ")
+lName = input("Enter the last name: ")
+fullName = f"{fName} {lName}"
+print(f"Hello, {fullName}! Welcome to the Python program.")
